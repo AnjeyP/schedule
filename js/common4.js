38a367f467458daa1d95d4e1daec0fffdd6728ca
Andrii Parfeniuk <and0parf@gmail.com>
2016-11-25 00:05:03 +0200

    added vacation check

--- js/common4.js
+++ js/common4.js
@@ -20,56 +20,56 @@ function init(){
 	//generating working schedule '1by2' for employee
 	td.each(function(index){
 			//checking first row and column to be not allowed to addClass
-			
+			//jan
 			if ((index-32)%3===0	&& index>31 && index<63) $(this).addClass("user1");
 			if ((index-33)%3===0	&& index>31 && index<63) $(this).addClass("user2");
 			if ((index-34)%3===0 && index>31 && index<63) $(this).addClass("user3");
-
+			//feb
 			if ((index-64)%3===0	&& index>63 && index<93) $(this).addClass("user2");
 			if ((index-65)%3===0	&& index>63 && index<93) $(this).addClass("user3");
 			if ((index-66)%3===0 && index>63 && index<93) $(this).addClass("user1");
-
+			//march
 			if ((index-96)%3===0	&& index>95 && index<127) $(this).addClass("user1");
 			if ((index-97)%3===0	&& index>95 && index<127) $(this).addClass("user2");
 			if ((index-98)%3===0 && index>95 && index<127) $(this).addClass("user3");
-
+			//april
 			if ((index-128)%3===0	&& index>127 && index<158) $(this).addClass("user2");
 			if ((index-129)%3===0	&& index>127 && index<158) $(this).addClass("user3");
 			if ((index-130)%3===0 && index>127 && index<158) $(this).addClass("user1");
-
+			//may
 			if ((index-160)%3===0	&& index>159 && index<191) $(this).addClass("user2");
 			if ((index-161)%3===0	&& index>159 && index<191) $(this).addClass("user3");
 			if ((index-162)%3===0 && index>159 && index<191) $(this).addClass("user1");
-
+			//june
 			if ((index-192)%3===0	&& index>191 && index<222) $(this).addClass("user3");
 			if ((index-193)%3===0	&& index>191 && index<222) $(this).addClass("user1");
 			if ((index-194)%3===0 && index>191 && index<222) $(this).addClass("user2");
-
+			//july
 			if ((index-224)%3===0	&& index>223 && index<255) $(this).addClass("user3");
 			if ((index-225)%3===0	&& index>223 && index<255) $(this).addClass("user1");
 			if ((index-226)%3===0 && index>223 && index<255) $(this).addClass("user2");
-
+			//august
 			if ((index-256)%3===0	&& index>255 && index<287) $(this).addClass("user1");
 			if ((index-257)%3===0	&& index>255 && index<287) $(this).addClass("user2");
 			if ((index-258)%3===0 && index>255 && index<287) $(this).addClass("user3");
-
+			//september
 			if ((index-288)%3===0	&& index>287 && index<318) $(this).addClass("user2");
 			if ((index-289)%3===0	&& index>287 && index<318) $(this).addClass("user3");
 			if ((index-290)%3===0 && index>287 && index<318) $(this).addClass("user1");
-
+			//october
 			if ((index-320)%3===0	&& index>319 && index<351) $(this).addClass("user2");
 			if ((index-321)%3===0	&& index>319 && index<351) $(this).addClass("user3");
 			if ((index-322)%3===0 && index>319 && index<351) $(this).addClass("user1");
-
+			//november
 			if ((index-352)%3===0	&& index>351 && index<382) $(this).addClass("user3");
 			if ((index-353)%3===0	&& index>351 && index<382) $(this).addClass("user1");
 			if ((index-354)%3===0 && index>351 && index<382) $(this).addClass("user2");
-
+			//december
 			if ((index-384)%3===0	&& index>383 && index<415) $(this).addClass("user3");
 			if ((index-385)%3===0	&& index>383 && index<415) $(this).addClass("user1");
 			if ((index-386)%3===0 && index>383 && index<415) $(this).addClass("user2");
 
-	})
+		})
 
 	//determining the current date
 	var data = new Date();
@@ -111,8 +111,8 @@ function init(){
 		if (index==res-date) 	$(this).css('color','red');
 		//hardcode need to fix
 		if (index<=29 && ((index-4)%7==0 || (index-5)%7==0))
-					$(this).css("background-color","red");
-					else if (index<=30) $(this).css("background-color","pink");
+			$(this).css("background-color","red");
+		else if (index<=30) $(this).css("background-color","pink");
 	})
 
 	// switch employee by click
@@ -125,25 +125,25 @@ function init(){
 				&& index!==93 && index!==94 && index!==158 && index!==222 && index!==318 && 
 				index!==382 && index>=res){
 				count++;
-				if (count%2!==0 && count%3!==0) $(this).addClass("user1").removeClass("user2 user3");
-				else if (count%2===0 && count%4!==0) $(this).addClass("user2").removeClass("user1 user3");
-				else if (count%3===0) $(this).addClass("user3").removeClass("user1 user2");
-				/*if (count%4===0) $(this).removeClass("user1 user2 user3");*/
-			}
+			if (count%2!==0 && count%3!==0) $(this).addClass("user1").removeClass("user2 user3");
+			else if (count%2===0 && count%4!==0) $(this).addClass("user2").removeClass("user1 user3");
+			else if (count%3===0) $(this).addClass("user3").removeClass("user1 user2");
+			/*if (count%4===0) $(this).removeClass("user1 user2 user3");*/
+		}
 
-		})
+	})
 
 
 	})
 	//employee filtering
 	$("#filters").on('click', '.btn1', function(){
-	
+
 		td.filter('.user2').toggleClass('hide');
 		td.filter('.user3').toggleClass('hide');
 		$('.btn1').toggleClass('highlight_blue');
-	
+
 	}).on('click','.btn2', function(){
-	
+
 		td.filter('.user1').toggleClass('hide');
 		td.filter('.user3').toggleClass('hide');
 		$('.btn2').toggleClass('highlight_green');
@@ -153,8 +153,8 @@ function init(){
 		td.filter('.user2').toggleClass('hide');
 		$('.btn3').toggleClass('highlight_pink');
 	})
-//weekend's highlight
-//jan
+	//weekend's highlight
+	//jan
 	weekendMapping(31,30,1,2);
 	//feb
 	weekendMapping(63,28,5,6);
@@ -181,40 +181,40 @@ function init(){
 
 	//auxilary functions
 	function weekendMapping(a,b,c,d){
-			$(td[a]).click(function(){
-				$(this).css('color','red');
+		$(td[a]).click(function(){
+			$(this).css('color','red');
 				//to obtain universal array for comparising
 				var monthIndexArray = [31, 63, 95, 127, 159, 191, 223, 255, 287, 319, 351, 383];
 				var position = monthIndexArray.indexOf(a);
 				monthIndexArray.splice(position,1);
-			td.each(function(index){
-				for (i in monthIndexArray) if (index == monthIndexArray[i]) $(this).css('color','black');})
-			td.each(function(index){
+				td.each(function(index){
+					for (i in monthIndexArray) if (index == monthIndexArray[i]) $(this).css('color','black');})
+				td.each(function(index){
 					if (index<=b && ((index-c)%7==0 || (index-d)%7==0))
-					$(this).css("background-color","red");
+						$(this).css("background-color","red");
 					else if (index<=30) $(this).css("background-color","pink");
-		
-			})	
-		})
+
+				})	
+			})
 	}
 		//tabs-13 december
-			var tab2_td = $('#tabs-13').find('td');
-			var janDay=0;
-			var dayArrIndex=0;
-			var dayArr = ['mo', 'tu', 'we', 'th', 'fr', 'sa', 'su'];
-			tab2_td.each(function(index){
-				if (index>=0 && index<=30) $(this).text(++janDay).css("background-color","pink");
-				if (index>=0 && index<=30 && ((index-2)%7==0 || (index-3)%7==0)) $(this).css("background-color","red");
-				if (index>=32 && index<=62 && ((index-2)%7==0 || (index-3)%7==0)) $(this).css("background-color","red");
+		var tab2_td = $('#tabs-13').find('td');
+		var janDay=0;
+		var dayArrIndex=0;
+		var dayArr = ['mo', 'tu', 'we', 'th', 'fr', 'sa', 'su'];
+		tab2_td.each(function(index){
+			if (index>=0 && index<=30) $(this).text(++janDay).css("background-color","pink");
+			if (index>=0 && index<=30 && ((index-2)%7==0 || (index-3)%7==0)) $(this).css("background-color","red");
+			if (index>=32 && index<=62 && ((index-2)%7==0 || (index-3)%7==0)) $(this).css("background-color","red");
 
-				if (index>=32 && index<=62 && (index-32)%7==0) $(this).text('th').css("background-color","pink");
-				if (index>=32 && index<=62 && (index-33)%7==0) $(this).text('fr').css("background-color","pink");
-				if (index>=32 && index<=62 && (index-34)%7==0) $(this).text('sa').css("background-color","red");
-				if (index>=32 && index<=62 && (index-35)%7==0) $(this).text('su').css("background-color","red");
-				if (index>=32 && index<=62 && (index-36)%7==0) $(this).text('mo').css("background-color","pink");
-				if (index>=32 && index<=62 && (index-37)%7==0) $(this).text('tu').css("background-color","pink");
-				if (index>=32 && index<=62 && (index-38)%7==0) $(this).text('we').css("background-color","pink");
-			})
+			if (index>=32 && index<=62 && (index-32)%7==0) $(this).text('th').css("background-color","pink");
+			if (index>=32 && index<=62 && (index-33)%7==0) $(this).text('fr').css("background-color","pink");
+			if (index>=32 && index<=62 && (index-34)%7==0) $(this).text('sa').css("background-color","red");
+			if (index>=32 && index<=62 && (index-35)%7==0) $(this).text('su').css("background-color","red");
+			if (index>=32 && index<=62 && (index-36)%7==0) $(this).text('mo').css("background-color","pink");
+			if (index>=32 && index<=62 && (index-37)%7==0) $(this).text('tu').css("background-color","pink");
+			if (index>=32 && index<=62 && (index-38)%7==0) $(this).text('we').css("background-color","pink");
+		})
 
 			//generating working schedule '1by2' for employee
 			tab2_td.each(function(index){
@@ -222,7 +222,8 @@ function init(){
 				if(index>=98 && index<=126 && (index-98)%3==0) $(this).addClass('user2');
 				if(index>=129 && index<=158 && (index-129)%3==0) $(this).addClass('user1');
 			})
-
+			//user stats call
+			userStatistics();
 			// switch employee by click in month view tab
 			//user2
 			tab2_td.each(function(index){
@@ -234,6 +235,7 @@ function init(){
 
 						$(tab2_td[index-32]).removeClass('user3');
 						$(tab2_td[index+32]).removeClass('user1');
+						userStatistics();
 					}
 				})
 			})
@@ -245,6 +247,7 @@ function init(){
 						$(td[index+320]).addClass('user3').removeClass('user1 user2');
 						$(tab2_td[index+32]).removeClass('user2');
 						$(tab2_td[index+64]).removeClass('user1');
+						userStatistics();
 					}
 				})
 			})
@@ -256,9 +259,59 @@ function init(){
 						$(td[index+256]).addClass('user1').removeClass('user3 user2');
 						$(tab2_td[index-32]).removeClass('user2');
 						$(tab2_td[index-64]).removeClass('user3');
+						userStatistics();
 					}
 				})
 			})
+			//auxilary functions
+			//statistics per user
+			function userStatistics(){
+				var user3Count=0;
+				var user2Count=0;
+				var user1Count=0;
+				tab2_td.each(function(){
+					if ($(this).hasClass('user3')) $(tab2_td[95]).text(user3Count++);
+					if ($(this).hasClass('user2')) $(tab2_td[127]).text(user2Count++);
+					if ($(this).hasClass('user1')) $(tab2_td[159]).text(user1Count++);
+				})
+			}
+		//highlighting vacation days
+		$('#check').click(function(){
+			var startData = $('#datepicker').val();
+			var endData = $('#datepicker2').val();
+			var startVacation = Number(startData.split("")[3]+startData.split("")[4]);
+			var endVacation = Number(endData.split("")[3]+endData.split("")[4]);
+			var optionArr = $('select').children();
+			for (key in optionArr) if (optionArr[key].selected==true) var user = optionArr[key].value;
+			if (user =="User3")
+						tab2_td.each(function(index){
+								
+								if(index>=64+startVacation-1 && index<=64+endVacation-1) {
+									if ($(this).hasClass("vacation_month")) $(this).removeClass("vacation_month");
+									$(tab2_td[index]).addClass("vacation_month");
+									$(td[index+320]).addClass("vacation_year");
+								}
+							})	
+			if (user =="User2")
+						tab2_td.each(function(index){
+							
+							if(index>=96+(startVacation-1) && index<=96+(endVacation-1)) {
+								if ($(this).hasClass("vacation_month")) $(this).removeClass("vacation_month");
+								$(this).addClass("vacation_month");
+								$(td[index+288]).addClass("vacation_year");
+							}
+						})
+			if (user =="User1")
+						tab2_td.each(function(index){
+							
+							if(index>=128+(startVacation-1) && index<=128+(endVacation-1)) {
+								if ($(this).hasClass("vacation_month")) $(this).removeClass("vacation_month");
+								$(this).addClass("vacation_month");
+								$(td[index+256]).addClass("vacation_year");
+							}
+						})	
+		})
 		
+			
 
 }
